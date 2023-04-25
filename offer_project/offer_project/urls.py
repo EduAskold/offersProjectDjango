223d35c2981544c19f44b9978feb0ed7a6df7ba8
sbuzunova <sophia.buzunova@gmail.com>
2023-04-25 17:25:09 +0200

    create main page

--- offer_project/offer_project/urls.py
+++ offer_project/offer_project/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from offer_project import settings
 from django.conf.urls.static import static
+from offers.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main),
 ]+static (settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- offer_project/offers/admin.py
+++ offer_project/offers/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from offers.models import Offer, Category
 # Register your models here.
+admin.site.register([Offer, Category])
